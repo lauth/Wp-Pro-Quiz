be00906e9cf30ab861184a908c9e3b187fa24ff2
thomas <thomas@rezoactif.com>
2016-02-26 17:50:37 +0100

    add manage-categories

--- manage-categories.php
+++ manage-categories.php
@@ -1,2 +1,19 @@
 <?php
 
+function manage_categories_init()
+{
+    add_submenu_page(
+        'wpProQuiz',
+        __('Catégories', 'wp-pro-quiz'),
+        __('Catégories', 'wp-pro-quiz'),
+        'wpProQuiz_change_settings',
+        'wpProQuiz_categories',
+        'manage_categories_display');
+}
+
+add_action('admin_menu', 'manage_categories_init');
+
+function manage_categories_display()
+{
+    return 'tutu';
+}
